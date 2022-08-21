3c3b763eec5493aaa769e97357e7a4fb5fbfaac2
vonkulit <vonkulit@gmail.com>
2022-08-21 16:31:06 +0800

    Initial commit

---
+++ Assets/ChangingScenes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangingScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene (name);
+	}
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+    
+    
+     public void LoadMainMenu()
+     {  
+         SceneManager.LoadScene(0);
+     }
+     public void LoadSelectLevel()
+     {  
+         SceneManager.LoadScene(1);
+     }
+     public void LoadGameProper()
+     {  
+         SceneManager.LoadScene(2);
+     }
+}
